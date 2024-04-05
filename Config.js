8b3d85fa01d0a0da89e85b7171efb3411c618d9b
Bluedini1 <166182407+Bluedini1@users.noreply.github.com>
2024-04-06 00:11:33 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,18 +2,18 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownernumber = ['08074160608']
+global.ownername = "Bluedini 💙"//owner name
 global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.socialm = "GitHub: Bluedini"
+global.location = "Nigeria"
 
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+global.botname = 'YUTA-𝐌𝐃' //name of the bot
 
 //sticker details
-global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
+global.stickername = 'Bluedini💙'
 global.packname = 'Sticker By'
-global.author = 'Maria Bot'
+global.author = 'YUTA Bot'
 //console view/theme
 global.themeemoji = '🧩'
 global.wm = "Ayush botz inc."
